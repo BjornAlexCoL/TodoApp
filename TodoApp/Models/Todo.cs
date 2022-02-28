f5cea4ad8b888292ecc83750eff46bef2713c04c
Louis Lim <curtsky@outlook.com>
2022-02-28 16:50:39 +0100

    update Todo, TodoApp and Todotest. Person ID pending

--- TodoApp/Models/Todo.cs
+++ TodoApp/Models/Todo.cs
@@ -4,7 +4,33 @@ using System.Text;
 
 namespace TodoApp.Models
 {
-    class Todo
+    public class Todo
     {
+        static int toDoCounter = 0;
+
+        int taskId;
+
+        private string description;
+        private bool done;
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 1)
+                {
+                    throw new ArgumentException("Description should not be blank.");
+                }
+                description = value;
+            }
+        }
+
+        public Todo(string description, bool done)
+        {
+            this.taskId = ++toDoCounter;
+            this.description = description;
+            this.done = done;
+        }
     }
 }
--- TodoAppTest/Models/TodoTest.cs
+++ TodoAppTest/Models/TodoTest.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TodoApp.Models;
+using Xunit;
 
 namespace TodoAppTest.Models
 {
     class TodoTest
     {
+        public void ToDoConstructorTest()
+        {
+            //Arrange
+            string description = "Create week1 Assignment";
+            bool done = false;
+
+            //Act
+            Todo result = new Todo(description, done);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(description, result.Description);
+        }
     }
 }
