b208ef6542688a3ba2457902183c01fe435be58e
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2022-02-28 17:30:17 +0100

    Person made and tested. Not assigning names give nullvalue.

--- TodoApp/Models/Person.cs
+++ TodoApp/Models/Person.cs
@@ -4,7 +4,48 @@ using System.Text;
 
 namespace TodoApp.Models
 {
-    class Person
+    public class Person
     {
+        private int id;
+        private string firstName;
+        private string lastName;
+
+        public Person(int id)
+        {
+            this.id = id;
+        }
+        public Person(int id, string firstName, string lastName) : this(id)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public int Id
+        {
+            get => id;
+        }
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if ((value != null) && (value != ""))
+                {
+                    this.firstName = value;
+                }
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if ((value != null) && (value != ""))
+                {
+                    lastName = value;
+                }
+            }
+        }
+
     }
 }
--- TodoAppTest/Models/PersonTest.cs
+++ TodoAppTest/Models/PersonTest.cs
@@ -1,10 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TodoApp.Models;
+using Xunit;
 
 namespace TodoAppTest.Models
 {
-    class PersonTest
+    public class PersonTest
     {
+        [Theory]
+        [InlineData(1, 1)]
+        public void PersonIdTest(int id, int expected)
+        {
+            //Assign
+            Person person = new Person(id);
+            //Act
+            int result = person.Id;
+
+            //assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(1, "Björn", "Larsson", "Björn")]
+        [InlineData(1, "", "Larsson", null)]
+        [InlineData(2, "Björn", "Larsson", "Björn")]
+
+        public void PersonFirstNameTest(int id, string firstName, string lastName, string expected)
+        {
+            //Assign
+            Person personOne = new Person(id, firstName, lastName);
+            Person personTwo = new Person(id);
+
+            //Act
+            personTwo.FirstName = firstName;
+            string resultOne = personOne.FirstName;
+            string resultTwo = personTwo.FirstName;
+
+            //assert
+            Assert.Equal(expected, resultOne);
+            Assert.Equal(expected, resultTwo);
+        }
+        [Theory]
+        [InlineData(1, "Björn", "Larsson", "Larsson")]
+        [InlineData(1, "Björn", "", null)]
+        [InlineData(2, "Björn", "Larsson", "Larsson")]
+
+        public void PersonLastNameTest(int id, string firstName, string lastName, string expected)
+        {
+            //Assign
+            //Assign
+            Person personOne = new Person(id, firstName, lastName);
+            Person personTwo = new Person(id);
+
+            //Act
+            personTwo.LastName = lastName;
+            string resultOne = personOne.LastName;
+            string resultTwo = personTwo.LastName;
+
+            //assert
+            Assert.Equal(expected, resultOne);
+            Assert.Equal(expected, resultTwo);
+
+        }
     }
 }
