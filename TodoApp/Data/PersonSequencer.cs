10b13d6728d191bd810b861e1e3add15e7a8a434
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2022-03-01 09:47:01 +0100

    PersonSequencer done

--- TodoApp/Data/PersonSequencer.cs
+++ TodoApp/Data/PersonSequencer.cs
@@ -6,5 +6,16 @@ namespace TodoApp.Data
 {
     class PersonSequencer
     {
+        private static int personId = 0;
+
+        public static int NextPersonID()
+        {
+            return ++personId;
+        }
+
+        public static void Reset()
+        {
+            personId = 0;
+        }
     }
 }
